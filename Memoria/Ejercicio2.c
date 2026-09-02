23bcd1dc810c68fafa4d9ca203ad6fc383f37158
aliciabelaunde <mbelaunde@ucb.edu.bo>
2026-09-02 10:29:01 -0400

    Correccion codigo

--- Memoria/Ejercicio2.c
+++ Memoria/Ejercicio2.c
@@ -5,22 +5,24 @@ int main() {
     int longitud = sizeof(arreglo) / sizeof(arreglo[0]);
     int suma = 0;
 
-    // Bloque de ensamblador en línea (GCC / x86-64)
+    // Bloque de ensamblador en línea (GCC / x86-64 en sintaxis Intel)
     __asm__ (
-        "mov ecx, %[len]\n\t"        // ecx = contador del bucle (longitud)
-        "mov rsi, 0\n\t"            // rsi = índice i (0, 1, 2, ...)
-        "xor eax, eax\n\t"          // eax = 0 (acumulador para la suma)
+        ".intel_syntax noprefix\n\t"        // Activa la sintaxis Intel
+        "mov ecx, %[len]\n\t"               // ecx = longitud
+        "mov rsi, 0\n\t"                    // rsi = índice i
+        "xor eax, eax\n\t"                  // eax = 0
 
         "bucle_suma:\n\t"
         "add eax, dword ptr [%[arr] + rsi*4]\n\t" // suma += arreglo[i]
-        "inc rsi\n\t"               // i++
-        "loop bucle_suma\n\t"       // decrementa ecx y repite si ecx != 0
+        "inc rsi\n\t"                       // i++
+        "loop bucle_suma\n\t"               // decrementa ecx y repite si ecx != 0
 
-        "mov %[res], eax\n\t"       // guarda el resultado en la variable 'suma'
-        : [res] "=r" (suma)         // Salida: variable 'suma' de C
-        : [arr] "r" (arreglo),      // Entradas: puntero al arreglo
-          [len] "r" (longitud)      // y longitud
-        : "eax", "ecx", "rsi"       // Registros modificados
+        "mov %[res], eax\n\t"               // guarda el resultado en 'suma'
+        ".att_syntax prefix\n\t"            // Restaura la sintaxis AT&T por seguridad
+        : [res] "=r" (suma)
+        : [arr] "r" (arreglo),
+          [len] "r" (longitud)
+        : "eax", "ecx", "rsi", "cc"
     );
 
     printf("La suma del arreglo es: %d\n", suma);
